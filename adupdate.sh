97e470b0040b1651b85e0e32f575c0f6d6eb3d7d
kysdm <641349198@qq.com>
2016-12-27 08:13:24 +0800

    update

--- adupdate.sh
+++ adupdate.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
-#脚本作者Lean
+#脚本改自Lean
 
-wget --no-check-certificate --tries=5 -O /tmp/lazy.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
-wget --no-check-certificate --tries=5 -O /tmp/video.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
-#wget --tries=5 -O /tmp/user.action  http://update.adbyby.com/rule3/user.action
+wget --no-check-certificate -O /tmp/lazy.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
+wget --no-check-certificate -O /tmp/video.txt https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
+wget --no-check-certificate -O /tmp/user.action https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/user.action
 
 [ -s "/tmp/lazy.txt" ] && ( ! cmp -s /tmp/lazy.txt /usr/share/adbyby/data/lazy.txt ) && mv /tmp/lazy.txt /usr/share/adbyby/data/lazy.txt	
 [ -s "/tmp/video.txt" ] && ( ! cmp -s /tmp/video.txt /usr/share/adbyby/data/video.txt ) && mv /tmp/video.txt /usr/share/adbyby/data/video.txt	
-#[ -s "/tmp/user.action" ] && ( ! cmp -s /tmp/user.action /usr/share/adbyby/user.action ) && mv /tmp/user.action /usr/share/adbyby/user.action	
+[ -s "/tmp/user.action" ] && ( ! cmp -s /tmp/user.action /usr/share/adbyby/user.action ) && mv /tmp/user.action /usr/share/adbyby/user.action	
 
-#if [ -s "/tmp/lazy.txt" -a -s "/tmp/user.action" -a -s "/tmp/video.txt" ];then
-if [ -s "/tmp/lazy.txt" -a -s "/tmp/video.txt" ];then
-  echo "Adbyby Rules No Change"
- else
- #rm -f /usr/share/adbyby/update.info
-  touch /usr/share/adbyby/update.info
-  echo "Adbyby Rules Upadated!"
-fi
-
-#rm -f /tmp/lazy.txt /tmp/video.txt /tmp/user.action
-rm -f /tmp/lazy.txt /tmp/video.txt
+rm -f /tmp/lazy.txt /tmp/video.txt /tmp/user.action
 
 /etc/init.d/adbyby restart
+
 
 
