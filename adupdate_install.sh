21e33cf1782f2b3170d7f2f5b2a3875754863504
kysdm <gxk7231@gmail.com>
2017-09-22 19:20:58 +0800

    修复计划任务添加错误

--- adupdate_install.sh
+++ adupdate_install.sh
@@ -13,7 +13,7 @@ chmod 777 $ADBYBY/adupdate.sh
 #添加计划任务
 sed -i '/adupdate.sh/d' $crontab
 	echo '30 */6 * * * /usr/share/adbyby/adupdate.sh > /tmp/log/adupdate.log 2>&1' >> $crontab
-	crontab $crontab
+	/etc/init.d/cron restart
 
 #执行一次更新
 sh $ADBYBY/adupdate.sh
