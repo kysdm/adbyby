c11444fd8a3842e12b6166931e1f68266f058dfb
kysdm <gxk7231@gmail.com>
2017-09-22 18:04:20 +0800

    去除video文件更新

--- adupdate.sh
+++ adupdate.sh
@@ -1,53 +1,49 @@
 #!/bin/sh
 export ADBYBY=/usr/share/adbyby
 alias echo_date='echo 【$(date +%Y年%m月%d日\ %X)】:'
-url_main="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master"
-url_image="https://raw.githubusercontent.com/kysdm/adbyby/master/xwhyc-rules"
 
 #下载规则文件
 echo_date 下载规则文件中...
 
-wget -N --no-check-certificate -O /tmp/lazy.txt $url_main/lazy.txt
-  if [ "$?" == "0" ]; then
-    echo_date 下载lazy文件成功，继续下载video文件...
+wget --no-check-certificate -O /tmp/lazy.txt https://gitee.com/kysdm/xwhyc-rules/raw/master/lazy.txt
+ if [ "$?" == "0" ]; then
+    echo_date 下载lazy文件成功，因video文件无用跳过下载...
    else
-    echo_date 下载lazy文件失败，连接github失败，尝试挂代理后重试
+    echo_date 下载lazy文件失败，连接失败，尝试挂代理后重试
     rm -rf /tmp/lazy.txt
     exit
-  fi
+ fi
 
-wget -N --no-check-certificate -O /tmp/video.txt $url_image/video.txt
-  if [ "$?" == "0" ]; then
-    echo_date 下载video文件成功，判断规则是否有更新中...
-   else
-    echo_date 下载video文件失败，连接github失败，尝试挂代理后重试
-    rm -rf /tmp/video.txt
-    exit
-  fi
+#wget --no-check-certificate -O /tmp/video.txt https://gitee.com/kysdm/xwhyc-rules/raw/master/video.txt
+# if [ "$?" == "0" ]; then
+#    echo_date 下载video文件成功，判断规则是否有更新中...
+#   else
+#    echo_date 下载video文件失败，连接失败，尝试挂代理后重试
+#    rm -rf /tmp/video.txt
+# fi
 
 #wget -N --no-check-certificate -O /tmp/user.action $url_main/master/user.action
-
+#
 #判断是否有更新
 version_lazy_up=$(head -1 /tmp/lazy.txt  | awk -F' ' '{print $3,$4}')
-version_video_up=$(head -1 /tmp/video.txt | awk -F' ' '{print $3,$4}')
-
-version_lazy=$(head -1 $ADBYBY/lazy.txt  | awk -F' ' '{print $3,$4}')
-version_lazy=$(head -1 $ADBYBY/video.txt  | awk -F' ' '{print $3,$4}')
+#version_video_up=$(head -1 /tmp/video.txt | awk -F' ' '{print $3,$4}')
+version_lazy=$(head -1 $ADBYBY/data/lazy.txt  | awk -F' ' '{print $3,$4}')
+#version_lazy=$(head -1 $ADBYBY/data/video.txt  | awk -F' ' '{print $3,$4}')
 
 if [ "$version_lazy_up" != "$version_lazy" ];then
-  echo_date 本地lazy规则与云端规则相同，无需更新
- else
   echo_date 检测到lazy规则更新，应用规则中...
   mv /tmp/lazy.txt $ADBYBY/data/lazy.txt
+ else
+  echo_date 本地lazy规则与云端规则相同，无需更新
 fi
 
-if [ "$version_video_up" != "$version_video" ];then
-  echo_date 本地video规则与云端规则相同，无需更新
-  exit
-else
-  echo_date 检测到video规则更新，应用规则中...
-  mv /tmp/video.txt $ADBYBY/data/video.txt
-fi
+#if [ "$version_video_up" != "$version_video" ];then
+#  echo_date 检测到video规则更新，应用规则中...
+#  mv /tmp/video.txt $ADBYBY/data/video.txt
+#  exit
+#else
+#  echo_date 本地video规则与云端规则相同，无需更新
+#fi
 
 #if [ "$version_lazy_up" != "$version_lazy" -o （"$version_video_up" != "$version_video" ]
 
