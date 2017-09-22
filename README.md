35769d211efad0f13878b441aa0351448c49320f
kysdm <gxk7231@gmail.com>
2017-09-22 19:11:05 +0800

    更新说明

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# adbyby 广告屏蔽大师
+adbyby 路由版 辅助更新规则脚本
+
+适用于pandorabox openwrt LEDE 上所安装的“TuHao版luci-adbyby”
+不兼容其他固件
+
+用法：
+下载adupdate_install.sh脚本到路由，并执行
---
+++ adupdate_uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export ADBYBY=/usr/share/adbyby
+crontab=/etc/crontabs/root
+
+#删除wget插件
+# opkg remove wget
+
+#删除脚本文件
+  rm -f $ADBYBY/adupdate.sh 
+
+#删除计划任务
+  sed -i '/adupdate.sh/d' $crontab
+  /etc/init.d/cron restart
+
+
