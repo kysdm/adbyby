a3b94665dc7cda035ef0bbc2e14b57bb309e6c9d
kysdm <gxk7231@gmail.com>
2017-09-22 18:19:53 +0800

    精简脚本

--- adupdate.sh
+++ adupdate.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 export ADBYBY=/usr/share/adbyby
 alias echo_date='echo 【$(date +%Y年%m月%d日\ %X)】:'
+#规则地址，仅可启用一个，去除#启用该地址，镜像地址规则可能非最新
+#rules="https://gitee.com/kysdm/xwhyc-rules/raw/master/lazy.txt"   #镜像地址
+rules="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt"  #官方github地址
 
 #下载规则文件
 echo_date 下载规则文件中...
-
-wget --no-check-certificate -O /tmp/lazy.txt https://gitee.com/kysdm/xwhyc-rules/raw/master/lazy.txt
+echo_date 如长时间无反应，尝试挂代理后重试，或使用镜像地址
+wget -t10 -q --no-check-certificate -O /tmp/lazy.txt $rules
  if [ "$?" == "0" ]; then
     echo_date 下载lazy文件成功，因video文件无用跳过下载...
    else
@@ -14,16 +17,6 @@ wget --no-check-certificate -O /tmp/lazy.txt https://gitee.com/kysdm/xwhyc-rules
     exit
  fi
 
-#wget --no-check-certificate -O /tmp/video.txt https://gitee.com/kysdm/xwhyc-rules/raw/master/video.txt
-# if [ "$?" == "0" ]; then
-#    echo_date 下载video文件成功，判断规则是否有更新中...
-#   else
-#    echo_date 下载video文件失败，连接失败，尝试挂代理后重试
-#    rm -rf /tmp/video.txt
-# fi
-
-#wget -N --no-check-certificate -O /tmp/user.action $url_main/master/user.action
-#
 #判断是否有更新
 version_lazy_up=$(head -1 /tmp/lazy.txt  | awk -F' ' '{print $3,$4}')
 #version_video_up=$(head -1 /tmp/video.txt | awk -F' ' '{print $3,$4}')
@@ -35,20 +28,12 @@ if [ "$version_lazy_up" != "$version_lazy" ];then
   mv /tmp/lazy.txt $ADBYBY/data/lazy.txt
  else
   echo_date 本地lazy规则与云端规则相同，无需更新
+  rm -f /tmp/lazy.txt 
+  exit
 fi
 
-#if [ "$version_video_up" != "$version_video" ];then
-#  echo_date 检测到video规则更新，应用规则中...
-#  mv /tmp/video.txt $ADBYBY/data/video.txt
-#  exit
-#else
-#  echo_date 本地video规则与云端规则相同，无需更新
-#fi
-
-#if [ "$version_lazy_up" != "$version_lazy" -o （"$version_video_up" != "$version_video" ]
-
 #删除临时规则文件
-#rm -f /tmp/lazy.txt /tmp/video.txt /tmp/user.action
+rm -f /tmp/lazy.txt 
 
 #重启adbyby应用规则
 #/etc/init.d/adbyby restart
