c82023f979c8676ce59e05f7ba553dcc3e652840
kysdm <gxk7231@gmail.com>
2017-10-04 12:46:36 +0800

    修复因未按规范写造成的BUG

--- adupdate.sh
+++ adupdate.sh
@@ -2,11 +2,17 @@
 #kysdm(gxk7231@gmail.com)
 export ADBYBY=/usr/share/adbyby
 alias echo_date='echo 【$(date +%Y年%m月%d日\ %X)】:'
-
-sh_ver="1.0.1"
+judgment=$(sed -n '1p' $ADBYBY/create_jd.txt)
+sh_ver="1.0.3"
 
 #规则地址
 rules="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master"  #官方github地址
+
+#创建判断文件
+if [ ! -e "$ADBYBY/create_jd.txt" ]; then
+   touch $ADBYBY/create_jd.txt
+   echo "NO" >> $ADBYBY/create_jd.txt
+fi
 
 #下载规则文件
  echo_date -----------------------------------------------
@@ -20,7 +26,7 @@ rules="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master"  #官方gith
   else
      echo_date 下载lazy文件失败，连接失败，尝试挂代理后重试
      rm -rf /tmp/lazy.txt
-	 echo_date 脚本结束运行
+  	 echo_date 脚本结束运行
      exit 0
   fi
 
@@ -30,7 +36,7 @@ rules="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master"  #官方gith
   else
      echo_date 下载lazy文件失败，连接失败，尝试挂代理后重试
      rm -rf /tmp/video.txt
-	 echo_date 脚本结束运行
+  	 echo_date 脚本结束运行
      exit 0
   fi
 
@@ -40,36 +46,69 @@ rules="https://raw.githubusercontent.com/adbyby/xwhyc-rules/master"  #官方gith
  version_video_up=$(sed -n '1p' /tmp/video.txt | awk -F' ' '{print $3 $4}' | sed 's/-//g' | sed 's/://g')
  version_video=$(sed -n '1p' $ADBYBY/data/video.txt | awk -F' ' '{print $3 $4}' | sed 's/-//g' | sed 's/://g')
 
-
  if [ "$version_lazy_up" -le "$version_lazy" ];then
    echo_date 本地lazy规则已经最新，无需更新
     if [ "$version_video_up" -le "$version_video" ];then
        echo_date 本地video规则已经最新，无需更新
        rm -f /tmp/lazy.txt /tmp/video.txt
-	   echo_date 脚本结束运行
+	     echo_date 脚本结束运行
        exit 0
        else
-         echo_date 检测到video规则更新，应用规则中...
-         mv /tmp/video.txt $ADBYBY/data/video.txt
-         /etc/init.d/adbyby restart
-         rm -f /tmp/lazy.txt /tmp/video.txt
-		 echo_date 脚本结束运行
-         exit 0
+        if [ "$judgment" == "YES" ]; then
+          echo_date 检测到video规则更新，应用规则中...
+          chattr -i $ADBYBY/data/video.txt
+          cp -f /tmp/video.txt $ADBYBY/data/video.txt
+          chattr +i $ADBYBY/data/video.txt
+          /etc/init.d/adbyby restart
+          rm -f /tmp/lazy.txt /tmp/video.txt
+    	  	echo_date 脚本结束运行
+          exit 0
+        else
+          echo_date 检测到video规则更新，应用规则中...
+          cp -f /tmp/video.txt $ADBYBY/data/video.txt
+          /etc/init.d/adbyby restart
+          rm -f /tmp/lazy.txt /tmp/video.txt
+    	  	echo_date 脚本结束运行
+          exit 0
+        fi  
     fi
  else
-   echo_date 检测到lazy规则更新，应用规则中...
-   mv /tmp/lazy.txt $ADBYBY/data/lazy.txt
-    if [ "$version_video_up" -le "$version_video" ];then
-     echo_date 本地video规则已经最新，无需更新
-     rm -f /tmp/lazy.txt /tmp/video.txt
-     /etc/init.d/adbyby restart
-	 echo_date 脚本结束运行
-     exit 0
-    else
-     echo_date 检测到video规则更新，应用规则中...
-     mv /tmp/video.txt $ADBYBY/data/video.txt
-     /etc/init.d/adbyby restart
-     rm -f /tmp/lazy.txt /tmp/video.txt
-	 echo_date 脚本结束运行
-    fi
- fi
+   if [ "$judgment" == "YES" ]; then
+     echo_date 检测到lazy规则更新，应用规则中...
+     chattr -i $ADBYBY/data/lazy.txt
+     cp -f /tmp/lazy.txt $ADBYBY/data/lazy.txt
+     chattr +i $ADBYBY/data/lazy.txt
+     if [ "$version_video_up" -le "$version_video" ];then
+      echo_date 本地video规则已经最新，无需更新
+      rm -f /tmp/lazy.txt /tmp/video.txt
+      /etc/init.d/adbyby restart
+	    echo_date 脚本结束运行
+      exit 0
+     else
+      echo_date 检测到video规则更新，应用规则中...
+      chattr -i $ADBYBY/data/video.txt
+      cp -f /tmp/video.txt $ADBYBY/data/video.txt
+      chattr +i $ADBYBY/data/video.txt
+      /etc/init.d/adbyby restart
+      rm -f /tmp/lazy.txt /tmp/video.txt
+	    echo_date 脚本结束运行
+      exit 0
+     fi 
+   else
+    echo_date 检测到lazy规则更新，应用规则中...
+    cp -f /tmp/lazy.txt $ADBYBY/data/lazy.txt
+     if [ "$version_video_up" -le "$version_video" ];then
+       echo_date 本地video规则已经最新，无需更新
+       rm -f /tmp/lazy.txt /tmp/video.txt
+       /etc/init.d/adbyby restart
+	     echo_date 脚本结束运行
+       exit 0
+     else
+      echo_date 检测到video规则更新，应用规则中...
+      cp -f /tmp/video.txt $ADBYBY/data/video.txt
+      /etc/init.d/adbyby restart
+      rm -f /tmp/lazy.txt /tmp/video.txt
+	    echo_date 脚本结束运行
+     fi 
+   fi 
+  fi
