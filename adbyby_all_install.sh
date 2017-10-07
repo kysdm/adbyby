9d311f4b1af2b2882b248602d8d1fcfdb1f05745
kysdm <gxk7231@gmail.com>
2017-10-07 14:38:19 +0800

    perfect script

--- adbyby_all_install.sh
+++ adbyby_all_install.sh
@@ -14,15 +14,20 @@ Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 
 opkg list-installed | awk -F' ' '{print $1}' > /tmp/installed.txt
 
-sh_ver="1.1.6"
+sh_ver="1.1.8"
 
 check_adbyby_pid(){
 	PID=`ps |grep -v grep | grep adbyby |awk '{print $1}'`
 }
 Download_adupdate(){
     wget -t3 -T10 --no-check-certificate -O $ADBYBY/adupdate.sh $kysdm_github/master/adupdate.sh
-    chmod 777 $ADBYBY/adupdate.sh
-    echo -e "${Info} 下载成功"   #可增加判断机制
+     if [ "$?" == "0" ]; then
+      chmod 777 $ADBYBY/adupdate.sh
+      echo -e "${Info} 下载成功"  
+     else
+      echo -e "${Error} 下载失败" 
+      exit 1
+     fi  
 }
 Update_all_install(){
     echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
@@ -100,26 +105,40 @@ stop_adbyby(){
 }
 auto_adupdate_install(){
 	echo && echo -e "
+—————自动判断 测试功能
+ ${Green_font_prefix}1.${Font_color_suffix} 自动判断当前固件，并自动添加自动更新规则功能  
 —————未安装规则辅助更新脚本
- ${Green_font_prefix}1.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
+ ${Green_font_prefix}2.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
 —————已安装规则辅助更新脚本
- ${Green_font_prefix}2.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为可视化操控界面(新版pandorabox固件)
- ${Green_font_prefix}3.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
- ${Green_font_prefix}4.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为可视化操控界面(非新版pandorabox固件)
+ ${Green_font_prefix}3.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为可视化操控界面(新版pandorabox固件)
+ ${Green_font_prefix}4.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
+ ${Green_font_prefix}5.${Font_color_suffix} 添加自动更新规则功能，web计划任务界面为可视化操控界面(非新版pandorabox固件)
 ————————————
- ${Green_font_prefix}5.${Font_color_suffix} 重启crontab进程
- ${Green_font_prefix}6.${Font_color_suffix} 退出
+ ${Green_font_prefix}6.${Font_color_suffix} 重启crontab进程
+ ${Green_font_prefix}7.${Font_color_suffix} 退出
 ————————————" && echo
     read -p " 现在选择顶部选项 [1-6]: " input
     case $input in 
-	 1) auto_adupdate4_install;;
-	 2) auto_adupdate1_install;;
-	 3) auto_adupdate2_install;;
-     4) auto_adupdate3_install;;
-	 5) restart_crontab;;
-	 6) exit 0	;;
+     1）auto_adupdate_auto_install;;
+	 2) auto_adupdate4_install;;
+	 3) auto_adupdate1_install;;
+	 4) auto_adupdate2_install;;
+     5) auto_adupdate3_install;;
+	 6) restart_crontab;;
+	 7) exit 0	;;
 	 *) echo -e "${Error} 请输入正确的数字 [1-6]" && exit 1;;
     esac
+}
+auto_adupdate_auto_install(){
+    if [ ! -e "$cron" ]; then
+     if  grep -q pandorabox /etc/banner ; then
+       auto_adupdate1_install
+     else
+       auto_adupdate3_install    
+     fi
+    else
+       auto_adupdate4_install
+    fi  
 }
 auto_adupdate1_install(){
     sed -i '/adbyby/d' $crontab  #防止有两条相同计划任务在系统中
@@ -165,26 +184,43 @@ auto_adupdate4_install(){
 }
 auto_adupdate_uninstall(){
 	echo && echo -e "
+—————自动判断 测试功能
+ ${Green_font_prefix}1.${Font_color_suffix} 自动判断当前固件，并自动移除自动更新规则功能   
 —————未安装规则辅助更新脚本启用自动更新规则功能的
- ${Green_font_prefix}1.${Font_color_suffix} 删除自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
+ ${Green_font_prefix}2.${Font_color_suffix} 移除自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
 —————已安装规则辅助更新脚本启用自动更新规则功能的
- ${Green_font_prefix}2.${Font_color_suffix} 删除自动更新规则功能，web计划任务界面为可视化操控界面(新版pandorabox固件)
- ${Green_font_prefix}3.${Font_color_suffix} 删除自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
- ${Green_font_prefix}4.${Font_color_suffix} 删除自动更新规则功能，web计划任务界面为可视化操控界面(非新版pandorabox固件)
+ ${Green_font_prefix}3.${Font_color_suffix} 移除自动更新规则功能，web计划任务界面为可视化操控界面(新版pandorabox固件)
+ ${Green_font_prefix}4.${Font_color_suffix} 移除自动更新规则功能，web计划任务界面为一个框(常见界面需自己手打任务时间)
+ ${Green_font_prefix}5.${Font_color_suffix} 移除自动更新规则功能，web计划任务界面为可视化操控界面(非新版pandorabox固件)
 ————————————
- ${Green_font_prefix}5.${Font_color_suffix} 重启crontab进程
- ${Green_font_prefix}6.${Font_color_suffix} 退出
-————————————" && echo
-    read -p " 现在选择顶部选项 [1-6]: " input
+ ${Green_font_prefix}6.${Font_color_suffix} 重启crontab进程
+ ${Green_font_prefix}7.${Font_color_suffix} 退出
+———————————— 
+ $Tip 仅限删除本脚本添加的计划任务 " && echo
+    read -p " 现在选择顶部选项 [1-7]: " input
     case $input in 
-	 1) auto_adupdate4_uninstall;;
-	 2) auto_adupdate1_uninstall;;
-	 3) auto_adupdate2_uninstall;;
-     4) auto_adupdate3_uninstall;;
-	 5) restart_crontab;;
-	 6) exit 0	;;
-	 *) echo -e "${Error} 请输入正确的数字 [1-6]" && exit 1;;
+	 1) auto_adupdate_auto_uninstall;;    
+	 2) auto_adupdate2_uninstall;;
+	 3) auto_adupdate1_uninstall;;
+	 4) auto_adupdate2_uninstall;;
+     5) auto_adupdate3_uninstall;;
+	 6) restart_crontab;;
+	 7) exit 0	;;
+	 *) echo -e "${Error} 请输入正确的数字 [1-7]" && exit 1;;
     esac
+}
+auto_adupdate_auto_uninstall(){                                
+ if [ ! -e "$cron" ]; then
+  if  grep -q pandorabox /etc/banner ; then
+      auto_adupdate1_uninstall
+      sed -i '/adbyby/d' $crontab
+      /etc/init.d/cron restart
+  else
+     echo -e "${Error} 不支持当前固件，请手动删除计划任务"    
+  fi
+ else
+   auto_adupdate2_uninstall
+ fi  
 }
 auto_adupdate1_uninstall(){
 	if  grep -q adupdate $cron ; then
@@ -205,11 +241,6 @@ auto_adupdate2_uninstall(){
 auto_adupdate3_uninstall(){
     echo -e "${Error} 因没有对应固件无法做支持"
     echo -e "${Info} 请手动删除添加的计划任务"
-}
-auto_adupdate4_uninstall(){
-	sed -i '/adbyby/d' $crontab
-    /etc/init.d/cron restart 
-    echo -e "${Info} 删除成功"
 }
 adbyby_install(){
     echo && echo -e "
@@ -333,8 +364,18 @@ re_rule_server(){
     echo "NO" > $ADBYBY/create_jd.txt
     chattr -i $ADBYBY/data/lazy.txt
     chattr -i $ADBYBY/data/video.txt
-    opkg remove chattr                          #可增加选项决定是否删除
-    echo -e "${Info} 更改成功"
+		read -p "是否移除chattr插件(默认: n):" yn
+		[[ -z "${yn}" ]] && yn="n"
+        case $yn in 
+         y|Y) 
+         opkg remove chattr
+         echo -e "${Info} 已移除chattr插件并更改成功" 
+         exit 0
+         ;;
+         n|N)
+         echo -e "${Info} 保留chattr插件并更改成功" 
+         ;;
+        esac 
 }
 cat_lazy(){
     lazy_time=$(sed -n '1p' $ADBYBY/data/lazy.txt | awk -F' ' '{print $3,$4}')
@@ -367,15 +408,15 @@ menu_adupdate(){
 menu_auto_adupdate(){
     if [ -e "$cron" ]; then
       if  grep -q "adupdate" "$cron" || grep -q "adupdate" "$crontab"; then
-        echo -e " [ 已启动自动更新规则功能 ]"
+        echo -e " [ 已启用自动更新规则功能 ]"
      else
-         echo -e " [ 未启动自动更新规则功能 ]"
+         echo -e " [ 未启用自动更新规则功能 ]"
       fi 
     else
       if  grep -q "adupdate" "$crontab"; then
-         echo -e " [ 已启动自动更新规则功能 ]"  
+         echo -e " [ 已启用自动更新规则功能 ]"  
       else
-         echo -e " [ 未启动自动更新规则功能 ]"
+         echo -e " [ 未启用自动更新规则功能 ]"
       fi  
     fi
 }
